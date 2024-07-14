708d9edcb7acb7533a2e25992b639365bf02efca
JeremyJalpha <jeremyjalpha@gmail.com>
2024-07-14 18:25:19 +0200

    Update ConversationContxt.go

--- ConversationContxt.go
+++ ConversationContxt.go
@@ -5,19 +5,26 @@ import (
 	"time"
 )
 
+type Pricelist struct {
+	PrlstPreamble string
+	Catalogue     []CatalogueSelection
+}
+
 type ConversationContext struct {
 	UserInfo     UserInfo
 	UserExisted  bool
+	Pricelist    Pricelist
 	CurrentOrder CustomerOrder
 	MessageBody  string
 	DBReadTime   time.Time
 }
 
-func NewConversationContext(db *sql.DB, senderNumber, messagebody string, isAutoInc bool) *ConversationContext {
+func NewConversationContext(db *sql.DB, senderNumber, messagebody string, prlst Pricelist, isAutoInc bool) *ConversationContext {
 	userInfo, curOrder, userExisted := NewUserInfo(db, senderNumber, isAutoInc)
 	context := &ConversationContext{
 		UserInfo:     userInfo,
 		UserExisted:  userExisted,
+		Pricelist:    prlst,
 		CurrentOrder: curOrder,
 		MessageBody:  messagebody,
 		DBReadTime:   time.Now(),
