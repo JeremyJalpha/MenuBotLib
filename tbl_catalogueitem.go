1e48e7052c7a4c80ac196ecb46d56bf1d940fa88
JeremyJalpha <jeremyjalpha@gmail.com>
2024-07-14 19:13:52 +0200

    replaced GetCatalogueItemsFromDB with version from setup_test.go in test proj

--- tbl_catalogueitem.go
+++ tbl_catalogueitem.go
@@ -3,6 +3,7 @@ package menubotlib
 import (
 	"database/sql"
 	"fmt"
+	"strings"
 )
 
 // Define a custom type for PricingType
@@ -35,28 +36,59 @@ func (i *CatalogueItem) CatalogueItemAsAString() string {
 	return qA
 }
 
-// GetCatalogueItemsFromDB retrieves catalogue items from the database based on catalogueID.
-func GetCatalogueItemsFromDB(db *sql.DB, catalogueID string) ([]CatalogueItem, error) {
-	queryString := `SELECT catalogueID, catalogueitemID, "selection", "item", "options", pricingType FROM catalogueitem WHERE catalogueID = $1`
-	rows, err := db.Query(queryString, catalogueID)
+func InsertCatalogueItems(db *sql.DB, selections []CatalogueSelection) error {
+	insertStmt := `
+	INSERT INTO catalogueitem (catalogueID, catalogueitemID, "selection", "item", "options", pricingType)
+	VALUES (?, ?, ?, ?, ?, ?);`
+
+	for _, selection := range selections {
+		for _, item := range selection.Items {
+			options := strings.Join(item.Options, ", ")
+			_, err := db.Exec(insertStmt, item.CatalogueID, item.CatalogueItemID, selection.Preamble, item.Item, options, item.PricingType)
+			if err != nil {
+				return err
+			}
+		}
+	}
+
+	return nil
+}
+
+func GetCatalogueItemsFromDB(db *sql.DB, catalogueid string) ([]CatalogueItem, error) {
+	query := `
+	SELECT catalogueID, catalogueitemID, "selection", "item", "options", pricingType
+	FROM catalogueitem
+	WHERE catalogueID = $1;`
+
+	rows, err := db.Query(query, catalogueid)
 	if err != nil {
 		return nil, err
 	}
 	defer rows.Close()
 
-	var items []CatalogueItem
+	var rtnItems []CatalogueItem
+
 	for rows.Next() {
 		var item CatalogueItem
-		err := rows.Scan(&item.CatalogueItemID, &item.Selection, &item.Item, &item.Options, &item.PricingType)
+		var optionsStr string
+
+		err := rows.Scan(&item.CatalogueID, &item.CatalogueItemID, &item.Selection, &item.Item, &optionsStr, &item.PricingType)
 		if err != nil {
-			return nil, err
+			item = CatalogueItem{}
 		}
-		items = append(items, item)
+
+		if optionsStr != "" {
+			item.Options = strings.Split(optionsStr, ", ")
+		} else {
+			item.Options = nil
+		}
+
+		rtnItems = append(rtnItems, item)
 	}
 
 	if err := rows.Err(); err != nil {
 		return nil, err
 	}
 
-	return items, nil
+	return rtnItems, nil
 }
