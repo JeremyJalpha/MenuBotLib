d7355a56dfe298d9ed2d2b29bac5759311ae991c
JeremyJalpha <jeremyjalpha@gmail.com>
2025-01-03 17:46:43 +0200

    Fixed indentation in example

--- tbl_catalogueitem.go
+++ tbl_catalogueitem.go
@@ -72,10 +72,10 @@ func GetCatalogueItemsFromDB(db *sql.DB, catalogueid string) ([]CatalogueItem, e
 
 	var rtnItems []CatalogueItem
 
-//Example:
-// {"MenuIndications":[
-// {"ItemMenuNum":1,"ItemAmount":"12"},
-// {"ItemMenuNum":14,"ItemAmount":"10"}]}
+	// Example:
+	// {"MenuIndications":[
+	// {"ItemMenuNum":1,"ItemAmount":"12"},
+	// {"ItemMenuNum":14,"ItemAmount":"10"}]}
 
 	for rows.Next() {
 		var item CatalogueItem
